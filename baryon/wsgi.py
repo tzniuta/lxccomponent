224cc4aff2cde4359cd864db5ed66100c210b831
Cha Dong-Hwi <tzniuta@gmail.com>
2015-04-30 00:58:51 +0900

    api -> service -> model

--- baryon/wsgi.py
+++ baryon/wsgi.py
@@ -1,14 +1,29 @@
 from paste import deploy
-
+import eventlet
+import eventlet.wsgi
+import socket
 
 class WorkerProvider(object):
 
-    def __init__(self,app,service):
+    def __init__(self, app, service, host='0.0.0.0', port=0):
         self._service = service
         self._app = app
         self._server = None
+        self._pool = eventlet.GreenPool(2)
+        info = socket.getaddrinfo(host,
+                                  port,
+                                  socket.AF_UNSPEC,
+                                  socket.SOCK_STREAM)[0]
+        bind_addr = (host, port)
+        self._socket = eventlet.listen(bind_addr, family=host, backlog=128)
 
     def start(self):
+        dup_socket = self._socket
+        self._server = eventlet.spawn(
+            eventlet.wsgi.server,
+            dup_socket,
+            self._app
+        )
         print "get socket"
         print "launch process (eventlet.wsgi.server,spawn)..."
 
@@ -21,10 +36,15 @@ class WorkerProvider(object):
 
 class Server(object):
 
-    def __init__(self, name,app):
-        self.name = name
-        self.service = None
-        self.app = app
+    def __init__(self, name, app):
+
+        self._worker = WorkerProvider(self, self.app)
 
     def start(self):
-       service = WorkerProvider(self, self.app)
+       self._worker.start()
+
+    def stop(self):
+        self._worker.stop()
+
+    def wait(self):
+        self._worker.wait()
