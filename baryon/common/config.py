42271b4493869a23ed18e53bb20a3f55ad000da0
Cha Dong-Hwi <tzniuta@gmail.com>
2015-05-02 04:42:20 +0900

    enabled global variable _ for i18n

--- baryon/common/config.py
+++ baryon/common/config.py
@@ -2,7 +2,6 @@
 
 import os
 from oslo.config import cfg
-from baryon.openstack.common.gettextutils import _
 
 core_opts = [
     cfg.StrOpt('bind_host',
@@ -10,7 +9,7 @@ core_opts = [
                 help=_("The host IP to bind to")),
     cfg.IntOpt('bind_port',
                 default=30000,
-                help=_("The port to bind to")),
+                help="The port to bind to"),
     cfg.StrOpt('api_paste_config',
                 default="api-paste.ini",
                 help="The API paste config file to use")
@@ -20,23 +19,19 @@ CONF = cfg.CONF
 CONF.register_opts(core_opts)
 
 def init(argv):
-    # TODO: change version  string to pbr function
+    # TODO: change version string to pbr function
     CONF(argv[1:], project='baryon', version='0.1')
 
     load_loggin()
 
 
 def load_loggin():
-    # TODO: logging isn't working
 
-    import gettext
-    gettext.install('baryon')
-
-    from baryon.openstack.common import gettextutils
+    # TODO: logging isn't working (why?)
     from baryon.openstack.common import log as logging
-    gettextutils.install('baryon')
 
-    LOG = logging.getLogger(__name__)
     logging.setup("baryon")
+    LOG = logging.getLogger(__name__)
     LOG.info("Logging enabled!")
     LOG.info(_("Logging enabled!"))
+    # print "logging isn't working"
--- baryon/common/exception.py
+++ baryon/common/exception.py
@@ -1,22 +1,12 @@
-# TODO: Add _ to Error Message
-from baryon.openstack.common.gettextutils import _
-
-
 class BaryonException(Exception):
-    """Base Nova Exception
 
-    To correctly use this class, inherit from it and define
-    a 'msg_fmt' property. That msg_fmt will get printf'd
-    with the keyword arguments provided to the constructor.
-
-    """
-    error_msg = "An unknown exception occurred."
+    error_msg = _("An unknown exception occurred.")
     error_code = 500
 
     def __init__(self, message=None, **kwargs):
         self.emsg = self.error_msg % kwargs
 
-        # TODO: Add fatal exception handling
+        #TODO: Add fatal exception handling
 
         super(BaryonException, self).__init__(self.emsg)
 
