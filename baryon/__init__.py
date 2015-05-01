6adefe9a3a5662ede7605e2e296d25301dac5ff0
Cha Dong-Hwi <tzniuta@gmail.com>
2015-05-02 04:51:07 +0900

    enabled global variable _ for i18n

--- baryon/__init__.py
+++ baryon/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2015 Cha Dong-Hwi
 
-""" Load gettextutils until to enable global _ variable """
+""" Loading gettextutils until to enable global _ variable """
 from baryon.openstack.common import gettextutils
 gettextutils.install('baryon')
